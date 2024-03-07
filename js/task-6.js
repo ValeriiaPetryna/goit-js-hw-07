0ee2b1eeb011d0a8862929bf524ed9b3a4404b9c
ValeriiaPetryna <valeriapetrina1@gmail.com>
2024-03-07 08:16:13 +0200

    add task 6

--- js/task-6.js
+++ js/task-6.js
@@ -1,51 +1,44 @@
 
 // Створюємо елементи
-const controls = document.querySelector("controls");
+const controls = document.querySelector(".controls");
 const numberInput = document.querySelector("input");
 const createBtn = document.querySelector('[data-create]');
 const destroyBtn = document.querySelector('[data-destroy]');
 const boxesEl = document.querySelector("#boxes");
-
-// Даємо стилі
+// Додаємо стилі
 boxesEl.style.display = 'flex';
 boxesEl.style.flexWrap = 'wrap';
-boxesEl.style.alingItems = 'center';
+boxesEl.style.alignItems = 'center';
 boxesEl.style.marginTop = '30px';
-
-numberInput.addEventListener("input", toogleValue);
+numberInput.addEventListener("input", toggleValue);
 createBtn.addEventListener("click", () => {
-  createBoxes(number(numberInput.value));
+  createBoxes(Number(inputValue));
 });
-
 destroyBtn.addEventListener("click", destroyBoxes);
 let inputValue = 0;
-function toogleValue(event) {
+function toggleValue(event) {
   inputValue = event.currentTarget.value;
   console.log(inputValue);
-
 }
-
 function createBoxes(amount) {
+  numberInput.value = "";
   boxesEl.innerHTML = "";
-  let boxsize = 30;
-  let emptyArray = [];
+  let boxSize = 30;
   for (let i = 1; i <= amount; i += 1) {
-    const newDiv = `<div style = "width: ${boxsize}px; height: ${boxsize}px; background: ${getRandomHexColor()}"></div>`;
-    numbers.push(i);
-    newDiv.join("")
-    boxesEl.insertAdjacentHTML("beforeend", newDiv);
-    boxsize += 10;
+    const newDiv = document.createElement("div");
+    newDiv.style.width = `${boxSize}px`;
+    newDiv.style.height = `${boxSize}px`;
+    newDiv.style.background = getRandomHexColor();
+    boxesEl.appendChild(newDiv);
+    boxSize += 10;
   }
-  return
 }
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, '0')}`;
 }
-
 function destroyBoxes() {
   numberInput.value = "";
-  return (boxesEl.innerHTML = "");
+  boxesEl.innerHTML = "";
 }
